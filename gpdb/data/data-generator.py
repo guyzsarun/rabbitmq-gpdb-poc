51aad64d4412620ada444995af59770d9192e543
guyzsarun <sarun.nunt@gmail.com>
2021-10-12 17:50:01 +0700

    Add power column

--- gpdb/data/data-generator.py
+++ gpdb/data/data-generator.py
@@ -14,6 +14,7 @@ def generate_json(dataframe):
             "temperature_2": row["temperature_2"],
             "humidity": row["humidity"],
             "pm2.5": row["pm25"],
+            "power": row["power"],
         }
         json_dump = json.dumps(data)
         print(json_dump)
@@ -28,9 +29,10 @@ def main(timeslot, basetmp, csv):
 
     time = np.sin(np.arange(0, timeslot, 1))
     pm25 = np.random.normal(90, 15, timeslot)
-    humid = np.random.normal(50, 15, timeslot)
+    humid = np.random.normal(50, 5, timeslot)
 
-    temp2_fluc = np.random.normal(15, 3, timeslot)
+    temp2_fluc = np.random.normal(10, 3, timeslot)
+    power_fluc = np.random.normal(50,1, timeslot)
 
     # Add seasonal data
     max_temp = 1
@@ -39,8 +41,10 @@ def main(timeslot, basetmp, csv):
     temp_flux = np.random.rand(len(time)) * (3)
 
     temp = temp_flux + basetmp + seasonal
-    temp2_fluc = temp + temp2_fluc
+    temp2_fluc = temp - temp2_fluc
     pm25 = pm25 + seasonal_pm
+    power = power_fluc + temp
+
 
     date_index = index[: len(temp)]
 
@@ -49,6 +53,7 @@ def main(timeslot, basetmp, csv):
     df["temperature_2"] = pd.Series(temp2_fluc, index=df.index)
     df["humidity"] = pd.Series(humid, index=df.index)
     df["pm25"] = pd.Series(pm25, index=df.index)
+    df["power"] = pd.Series(power, index=df.index)
 
     if csv != None:
         df.to_csv(csv, index=False)
